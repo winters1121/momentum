dbf5cc20e5474f34bb88a3bc6c3a05ae0e36038d
winters1121 <wuk1121@gmail.com>
2021-07-21 12:08:00 +0900

    #2.16 recap

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const age = parseInt(prompt("How old are you?"));
 
 if (isNaN(age) || age < 0) {
-  // age < 0이란 조건이 없으면, 음수를 입력했을 때, you are too young이 출력된다. 그렇기에 OR 연산자를 활용할 수 있지. OR는 원화버튼과 shift 버튼을 동시에 눌러
   console.log("Please write a number");
 } else if (age < 18) {
   console.log("You are too young.");
 } else if (age >= 18 && age <= 50) {
-  // AND 연산자는 모든 구문이 참이여야 참, 하나라도 거짓이면 거짓 OR 연산자는 여러개 중 하나라도 참이면 참, 둘다 false여야 false
   console.log("You can drink!");
 } else if (age > 50 && age <= 80) {
   console.log("I recommend u. dont drink.");
+} else if (age === 100) {
+  // 코드는 위에서부터 아래로 읽히니까 지금 코드와 바로 밑의 코드는 중복되는 값이니, age가 100인 경우를 따로 적용하고 싶다면 위에다 써줘야 한다. 
+  console.log("whatever you want.");
 } else if (age > 80) {
   alert("You have the right to remain silent.");
 }
 
-// else는 선택적 사항. 필요 없으면 사용하지 않아도 돼
+// = 이 한개면 변수에 값을 저장하겠다. 할당하겠다. 라는 의미.
+// === 라면 변수와 그 값을 확인하는 것.
+// !== 은 그 값이 아니다. 즉, NOT이란 의미를 가진 연산자.
+
+// 그렇다면 복잡하게 조건을 걸 수 있을까? 당연히 가능.
+if ((a && b) || (c && d)) {
+} //의 경우, (c && d)가 먼저 계산이 이루어지고 true나 false값이 나오겠지. if문이 true여야 조건문이 시작될 수 있지.
+
+// 이제 HTML과 상호작용할 수 있는 파트로 넘어갈 수 있어.
