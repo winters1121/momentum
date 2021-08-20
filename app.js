5bcfaa01f12ec99eac217bb4cd38d277c9be1d02
winters1121 <wuk1121@gmail.com>
2021-08-20 16:31:44 +0900

    #3.8 CSS in JS

--- app.js
+++ app.js
@@ -1,38 +1,15 @@
 const h1 = document.querySelector("div.hello h1");
 
 function handleTitleClick() {
-  h1.style.color = "green";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (h1.style.color === "blue") {
+    h1.style.color = "tomato";
+  } else {
+    h1.style.color = "blue";
+  } // h1.style.color를 여러번 열거하는 것보단 변수에 저장하는 것이 좀더 깔끔.
+  h1.style.color = newColor; // 이렇게 CSS를 JS에서 표현할 수도 있지만, 니코쌤은 CSS에서 사용하는 게 더 나으니, 다음 시간에 다시 알려줄 계획.
 }
 
-function handleTitleOver() {
-  h1.innerHTML = "mouse is over!";
-}
-function handleTitleOut() {
-  h1.innerHTML = "Mouse is gone!";
-}
-
-function handleTitleDbClick() {
-  h1.style.border = "2px dotted black";
-}
-function handleResize() {
-  document.body.style.backgroundColor = "tomato";
-} // h1은 document.title하곤 완전히 달라. body, head, title 태그의 경우 document에서 중요하기에 디폴트로 존재하기에 데려올 수 있는 것이고! 나머지 element들은 querySelector로 가져와야 해!
-function handleCopy() {
-  alert("어머 어딜만져!");
-}
-function handleOffline() {
-  alert("She's gone");
-}
-function handleOnline() {
-  alert("Winter is coming!");
-}
 h1.addEventListener("click", handleTitleClick);
 // h1.onclick = handleTitleClick 로 표현가능! 즉, 2가지로 event사용 가능하다. 근데 니코쌤은 전자의 방식을 더 선호.
-h1.addEventListener("mouseover", handleTitleOver);
-h1.addEventListener("mouseout", handleTitleOut);
-h1.addEventListener("dblclick", handleTitleDbClick);
-
-window.addEventListener("resize", handleResize);
-window.addEventListener("copy", handleCopy);
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
